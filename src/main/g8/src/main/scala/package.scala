8aabdb1a4be6150764b6b473b94c158cdd47492a
Daniel Spiewak <djspiewak@gmail.com>
2018-04-19 10:37:25 -0600

    Updated copyright year

--- src/main/g8/src/main/scala/package.scala
+++ src/main/g8/src/main/scala/package.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 $copyright_owner$
+ * Copyright 2018 $copyright_owner$
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
